0e98ce5a52653c07cd93029d42d22468360058cf
Leo <61775738+Leobej@users.noreply.github.com>
2022-05-20 23:02:24 +0300

    Added folder structure

---
+++ Hotel Management Project/Hotel Management Project/ViewModel/NotifyVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Management_Project.ViewModel
+{
+    public class NotifyVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged(string prop)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
